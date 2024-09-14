16c5ef2bfe505c71871990b68745719fdb716a1b
Leovisvuong <141708660+Leovisvuong@users.noreply.github.com>
2024-09-14 15:19:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿const double Pi = 3.14159;
+const string CompanyName = "Leovis Games";
+const int MaxRetries = 3;
+const double Gravity = 7.2;
+const int MonthsInYear = 12;
+int enemy_health = 100;
+double game_currency = 2500.5;
+string player_name = "John Doe";
+bool isalive = true;
+char patrol_state = 'S';
+byte player_age = 25;
+long score = 1000000000;
+decimal price = 100.5m;
+float speed = 100.5f;
+Console.WriteLine(enemy_health+"\n"+game_currency+"\n"+player_name+"\n"+isalive+"\n"+patrol_state+"\n"+player_age+"\n"+score+"\n"+price+"\n"+speed);
+Console.Write(Pi+"\n"+CompanyName+"\n"+MaxRetries+"\n"+Gravity+"\n"+MonthsInYear);
